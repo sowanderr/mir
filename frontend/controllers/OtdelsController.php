aa1fd89da86fff6641a9f5696cfbd5d30e7bedc1
Sowander <sowander1986@gmail.com>
2016-01-29 12:36:07 +0600

    пилим ACtiveRecord

--- frontend/controllers/OtdelsController.php
+++ frontend/controllers/OtdelsController.php
@@ -1,37 +1,121 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tolkushkin
- * Date: 28.01.16
- * Time: 14:24
- */
 
 namespace frontend\controllers;
 
-use yii\web\Controller;
-use yii\data\Pagination;
-use app\models\Otdels;
 use Yii;
+use app\models\Otdels;
+use frontend\models\OtdelsSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
+/**
+ * OtdelsController implements the CRUD actions for Otdels model.
+ */
 class OtdelsController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Otdels models.
+     * @return mixed
+     */
     public function actionIndex()
     {
-        $query = Otdels::find();
-
-        $pagination = new Pagination([
-            'defaultPageSize' => 10,
-            'totalCount' => $query->count(),
-        ]);
-
-        $otdels = $query->orderBy('name')
-            ->offset($pagination->offset)
-            ->limit($pagination->limit)
-            ->all();
+        $searchModel = new OtdelsSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'countries' => $otdels,
-            'pagination' => $pagination,
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
         ]);
+    }
+
+    /**
+     * Displays a single Otdels model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Otdels model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Otdels();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Otdels model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Otdels model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Otdels model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Otdels the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Otdels::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
     }
 }
--- frontend/views/otdels/index.php
+++ frontend/views/otdels/index.php
@@ -1,21 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tolkushkin
- * Date: 28.01.16
- * Time: 14:28
- */
-use yii\helpers\Html;
-use yii\widgets\LinkPager;
-?>
-    <h1>Отделы</h1>
-    <ul>
-        <?php foreach ($otdels as $otdel): ?>
-            <li>
-                <?= Html::encode("{$otdel->name} ({$otdels->id})") ?>:
-                <?= $otdels->long_name ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
 
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\OtdelsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Otdels';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="otdels-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Otdels', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'long_name',
+            'status',
+            'tehpol',
+            // 'update_time',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
