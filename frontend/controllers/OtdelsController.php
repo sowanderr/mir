fe5f9bca27ed044874465c447957564397a3539d
Sowander <sowander1986@gmail.com>
2016-01-29 13:19:11 +0600

    пилим ACtiveRecord

--- frontend/controllers/OtdelsController.php
+++ frontend/controllers/OtdelsController.php
@@ -4,7 +4,7 @@ namespace frontend\controllers;
 
 use Yii;
 use app\models\Otdels;
-use frontend\models\OtdelsSearch;
+use app\models\OtdelsSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
--- frontend/models/Otdels.php
+++ frontend/models/Otdels.php
@@ -1,24 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tolkushkin
- * Date: 28.01.16
- * Time: 14:18
- */
+
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Otdels extends ActiveRecord
+/**
+ * This is the model class for table "otdels".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $long_name
+ * @property integer $status
+ * @property integer $tehpol
+ * @property string $update_time
+ */
+class Otdels extends \yii\db\ActiveRecord
 {
-    const STATUS_INACTIVE = 0;
-    const STATUS_ACTIVE = 1;
-
     /**
-     * @return string название таблицы, сопоставленной с этим ActiveRecord-классом.
+     * @inheritdoc
      */
     public static function tableName()
     {
         return 'otdels';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'long_name'], 'required'],
+            [['status', 'tehpol'], 'integer'],
+            [['update_time'], 'safe'],
+            [['name', 'long_name'], 'string', 'max' => 255],
+            [['long_name'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'long_name' => 'Long Name',
+            'status' => 'Status',
+            'tehpol' => 'Tehpol',
+            'update_time' => 'Update Time',
+        ];
     }
 }
